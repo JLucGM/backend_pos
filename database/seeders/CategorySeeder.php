e095925b7c97845a8dd61d161770ea9516f5fdb5
Jean Gouirand <elluc09@gmail.com>
2025-03-10 17:10:49 -0400

    implementacion de nuevas categorias en CategorySeeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -13,40 +13,28 @@ class CategorySeeder extends Seeder
     public function run(): void
     {
         $categorias = [
-            [
-                'category_name' => 'Sin categoría',
-            ],
-            [
-                'category_name' => 'Ropa',
-            ],
-            [
-                'category_name' => 'Calzado',
-            ],
-            [
-                'category_name' => 'Electrónica',
-            ],
-            [
-                'category_name' => 'Hogar',
-            ],
-            [
-                'category_name' => 'Belleza',
-            ],
-            [
-                'category_name' => 'Alimentos',
-            ],
-            [
-                'category_name' => 'Deportes',
-            ],
-            [
-                'category_name' => 'Automotriz',
-            ],
-            [
-                'category_name' => 'Juguetes',
-            ],
-            [
-                'category_name' => 'Libros y papelería',
-            ],
-            // Agrega más categorías aquí
+            ['category_name' => 'Sin categoría',],
+            ['category_name' => 'Alimentos',],
+            ['category_name' => 'Automotriz',],
+            ['category_name' => 'Articulos para animales y mascotas',],
+            ['category_name' => 'Articulos deportivos',],
+            ['category_name' => 'Arte y entretenimiento',],
+            ['category_name' => 'Bebé y niño pequeño',],
+            ['category_name' => 'Salud y belleza',],
+            ['category_name' => 'Cámaras y óptica',],
+            ['category_name' => 'Calzado',],
+            ['category_name' => 'Equipaje y bolsos',],
+            ['category_name' => 'Ropa y accesorios',],
+            ['category_name' => 'Electrónica',],
+            ['category_name' => 'Hogar',],
+            ['category_name' => 'Deportes',],
+            ['category_name' => 'Juguetes',],
+            ['category_name' => 'Material de oficina',],
+            ['category_name' => 'Muebles',],
+            ['category_name' => 'Hardware',],
+            ['category_name' => 'Software',],
+            ['category_name' => 'Libros y papelería',],
+            ['category_name' => 'Vehículos y repuestos',],
         ];
 
         foreach ($categorias as $categoriaData) {
