860dc9dcd7de8314726944bbf331b56b6a097e8f
Jean Gouirand <elluc09@gmail.com>
2025-05-08 17:10:27 -0400

    arreglo

--- resources/js/Components/ui/sidebar.jsx
+++ resources/js/Components/ui/sidebar.jsx
@@ -7,16 +7,16 @@ import { PanelLeft } from "lucide-react"
 import { useIsMobile } from "@/hooks/use-mobile"
 import { cn } from "@/lib/utils"
 // import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Separator } from "@/components/ui/separator"
-import { Sheet, SheetContent } from "@/components/ui/sheet"
-import { Skeleton } from "@/components/ui/skeleton"
+import { Input } from "@/Components/ui/input"
+import { Separator } from "@/Components/ui/separator"
+import { Sheet, SheetContent } from "@/Components/ui/sheet"
+import { Skeleton } from "@/Components/ui/skeleton"
 import {
   Tooltip,
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
-} from "@/components/ui/tooltip"
+} from "@/Components/ui/tooltip"
 import { Button } from "@/Components/ui/button";
 
 const SIDEBAR_COOKIE_NAME = "sidebar:state"
