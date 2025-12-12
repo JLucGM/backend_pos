8585c866f7d124a07a610c4477c299f97cdf41db
Jean Gouirand <elluc09@gmail.com>
2025-12-12 17:28:57 -0400

    arreglo de pageseeder

--- database/seeders/PageSeeder.php
+++ database/seeders/PageSeeder.php
@@ -16,13 +16,44 @@ class PageSeeder extends Seeder
 
         $pages = [
             [
+                'title' => 'Inicio',
+                'content' => '<p>Explora nuestros productos destacados.</p>',
+                'is_default' => true,
+                'theme_id' => 1,
+                'uses_template' => true,
+                'template_id' => 3,
+                'company_id' => 1, // Asumiendo que tienes un owner con ID 1
+            ],
+            [
                 'title' => 'Tienda',
                 'content' => '<p>Explora nuestros productos destacados.</p>',
-                // 'layout' => json_encode([
-                //     [[
-                //         ["id"=>1764879462706,"type"=>"product","content"=>["columns"=>3,"gapX"=>"10px","gapY"=>"10px","backgroundColor"=>"#ffffff","limit"=>8,"children"=>[["id"=>1764879462707,"type"=>"productTitle","content"=>"Productos Destacados","styles"=>["layout"=>"fit","alignment"=>"center","color"=>"#000000","fontSize"=>"24px","fontWeight"=>"bold"]],["id"=>1764879462708,"type"=>"productCard","content"=>["cardBorder"=>"none","cardBorderThickness"=>"1px","cardBorderOpacity"=>"1","cardBorderRadius"=>"0px","cardPaddingTop"=>"0px","cardPaddingRight"=>"0px","cardPaddingBottom"=>"0px","cardPaddingLeft"=>"0px","children"=>[["id"=>1764879462709,"type"=>"productImage","content"=>"","styles"=>["aspectRatio"=>"square","imageBorder"=>"none","imageBorderThickness"=>"1px","imageBorderOpacity"=>"1","imageBorderRadius"=>"0px"]],["id"=>1764879462710,"type"=>"productName","content"=>"","styles"=>["layout"=>"fit","alignment"=>"left","color"=>"#000000","fontSize"=>"16px","fontWeight"=>"600"]],["id"=>1764879462711,"type"=>"productPrice","content"=>"","styles"=>["layout"=>"fit","alignment"=>"left","color"=>"#666666","fontSize"=>"14px","fontWeight"=>"normal"]]]],"styles"=>[]]]],"styles"=>[]]
-                //     ]]
-                // ]),
+                'is_default' => true,
+                'theme_id' => 1,
+                'uses_template' => true,
+                'template_id' => 3,
+                'company_id' => 1, // Asumiendo que tienes un owner con ID 1
+            ],
+            [
+                'title' => 'Detalles del producto',
+                'content' => '<p>Explora nuestros productos destacados.</p>',
+                'is_default' => true,
+                'theme_id' => 1,
+                'uses_template' => true,
+                'template_id' => 3,
+                'company_id' => 1, // Asumiendo que tienes un owner con ID 1
+            ],
+            [
+                'title' => 'Carrito de compras',
+                'content' => '<p>Explora nuestros productos destacados.</p>',
+                'is_default' => true,
+                'theme_id' => 1,
+                'uses_template' => true,
+                'template_id' => 3,
+                'company_id' => 1, // Asumiendo que tienes un owner con ID 1
+            ],
+            [
+                'title' => 'Checkout',
+                'content' => '<p>Explora nuestros productos destacados.</p>',
                 'is_default' => true,
                 'theme_id' => 1,
                 'uses_template' => true,
@@ -32,14 +63,7 @@ class PageSeeder extends Seeder
             [
                 'title' => 'Politicas de privacidad',
                 'content' => '<p>Explora nuestros productos destacados.</p>',
-                // 'layout' => json_encode([
-                //     [[
-                //         "id" => 1764870895545,
-                //         "type" => "pageContent",
-                //         "content" => null,
-                //         "styles" => []
-                //     ]]
-                // ]),
+                
                 'is_default' => true,
                 'theme_id' => 1,
                 'company_id' => 1, // Asumiendo que tienes un owner con ID 1
@@ -47,14 +71,6 @@ class PageSeeder extends Seeder
             [
                 'title' => 'Terminos de servicio',
                 'content' => '<p>Descuentos increíbles en productos seleccionados.</p>',
-                // 'layout' => json_encode([
-                //     [[
-                //         "id" => 1764870895545,
-                //         "type" => "pageContent",
-                //         "content" => null,
-                //         "styles" => []
-                //     ]]
-                // ]),
                 'is_default' => true,
                 'theme_id' => 1,
                 'company_id' => 1,
@@ -62,14 +78,6 @@ class PageSeeder extends Seeder
             [
                 'title' => 'Politicas de envio',
                 'content' => '<p>Descuentos increíbles en productos seleccionados.</p>',
-                // 'layout' => json_encode([
-                //     [[
-                //         "id" => 1764870895545,
-                //         "type" => "pageContent",
-                //         "content" => null,
-                //         "styles" => []
-                //     ]]
-                // ]),
                 'is_default' => true,
                 'theme_id' => 1,
                 'company_id' => 1,
@@ -77,14 +85,6 @@ class PageSeeder extends Seeder
             [
                 'title' => 'Información de contacto',
                 'content' => '<p>Descuentos increíbles en productos seleccionados.</p>',
-                // 'layout' => json_encode([
-                //     [[
-                //         "id" => 1764870895545,
-                //         "type" => "pageContent",
-                //         "content" => null,
-                //         "styles" => []
-                //     ]]
-                // ]),
                 'is_default' => true,
                 'theme_id' => 1,
                 'company_id' => 1,
